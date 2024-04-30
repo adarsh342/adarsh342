481379e0512cb9cb4bef5bfc0159a85f18a681f8
adarsh342 <126852443+adarsh342@users.noreply.github.com>
2024-04-30 11:12:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+// JavaScript
+function composeEmail() {
+    // Specify the recipient email address
+    var recipient = 'example@gmail.com';
+
+    // Specify the email subject and body
+    var subject = 'Regarding Job Opportunity';
+    var body = 'Hello,\n\nI am interested in discussing job opportunities with you.';
+
+    // Encode special characters in the email body
+    body = encodeURIComponent(body);
+
+    // Construct the mailto URL with recipient, subject, and body
+    var mailtoUrl = 'mailto:' + recipient + '?subject=' + subject + '&body=' + body;
+
+    // Open the mail client with the pre-filled email
+    window.open(mailtoUrl);
+}
+
+
 const hamburger = document.getElementById('hamburger'); 
 const menu = document.querySelector('.menu'); 
 
